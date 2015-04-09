6fabe2548bcc49fe2fb5672ba79c3971a8a430da
nvcleemp <nico.vancleemput@gmail.com>
2015-04-09 14:53:41 +0200

    Files to create the necessary database tables

---
+++ database/users.sql
@@ -0,0 +1,22 @@
+-- 
+-- Creates table that stores the user of the website
+-- 
+
+CREATE TABLE IF NOT EXISTS `users` (
+        `id` int(10) unsigned NOT NULL,
+        `email` varchar(45) DEFAULT NULL,
+        `firstName` varchar(45) DEFAULT NULL,
+        `lastName` varchar(45) DEFAULT NULL,
+        `username` varchar(45) NOT NULL,
+        `password` varchar(45) NOT NULL,
+        `isAdmin` tinyint(1) DEFAULT '0'
+);
+
+-- Set the indices for this table
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+-- id should auto-increment
+ALTER TABLE `users`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
