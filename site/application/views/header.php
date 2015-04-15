ae581670e6f681959c9b32a5d7c596b7d2937239
nvcleemp <nico.vancleemput@gmail.com>
2015-04-15 08:13:09 +0200

    Include support for sortable tables in the header (if requested)

--- site/application/views/header.php
+++ site/application/views/header.php
@@ -9,6 +9,14 @@ $this->lang->load('header');
 	<title><?php if (isset($title) && $title) { echo $title; } else { echo $this->lang->line('header.default.title'); } ?></title>
 
 	<?php echo link_tag(base_url().'css/main.css'); ?>
+        
+        <?php if (isset($sortable_tables) && $sortable_tables) { ?>
+        
+        <!-- Support for sortable tables -->
+        <script src="<?php echo base_url(); ?>js/sorttable.js"></script>
+	<?php echo link_tag(base_url().'css/sortable_table.css'); ?>
+        
+        <?php } ?>
 </head>
 <body>
 
