56f7b34a5e509fc6cc9dfc584e951f79d2feea77
nvcleemp <nico.vancleemput@gmail.com>
2015-04-15 09:57:49 +0200

    Added members to menu

--- site/application/language/dutch/menu_lang.php
+++ site/application/language/dutch/menu_lang.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('Directe toegang tot scripts is niet toegestaan');
 
+$lang['menu.members'] = 'Leden';
 $lang['menu.settings'] = 'Instellingen';
 $lang['menu.signout'] = 'Afmelden';
--- site/application/language/english/menu_lang.php
+++ site/application/language/english/menu_lang.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$lang['menu.members'] = 'Members';
 $lang['menu.settings'] = 'Settings';
 $lang['menu.signout'] = 'Sign out';
