9058589c99fbc92986129ad2e177c20178e86f49
nvcleemp <nico.vancleemput@gmail.com>
2015-04-18 09:28:53 +0200

    Added future item to menu

--- site/application/language/dutch/menu_lang.php
+++ site/application/language/dutch/menu_lang.php
@@ -2,6 +2,7 @@
 defined('BASEPATH') OR exit('Directe toegang tot scripts is niet toegestaan');
 
 $lang['menu.members'] = 'Leden';
+$lang['menu.memberships'] = 'Lidmaatschappen';
 $lang['menu.admin'] = 'Admin';
 $lang['menu.settings'] = 'Instellingen';
 $lang['menu.signout'] = 'Afmelden';
--- site/application/views/menu.php
+++ site/application/views/menu.php
@@ -9,6 +9,7 @@ $this->lang->load('menu');
 <div id="menu-container">
 <ul class="navigation-menu">
    <li class="navigation-menu-item"><a href="<?php echo site_url('members/overview'); ?>" class="navigation-item"><?php echo $this->lang->line('menu.members'); ?></a></li>
+   <li class="navigation-menu-item"><a href="" class="navigation-item"><?php echo $this->lang->line('menu.memberships'); ?></a></li>
    <li class="navigation-menu-item-right"><a href="<?php echo site_url('login/logout'); ?>" class="navigation-item"><?php echo $this->lang->line('menu.signout'); ?></a></li>
    <li class="navigation-menu-item-right"><a href="<?php echo site_url('user/settings'); ?>" class="navigation-item"><?php echo $this->lang->line('menu.settings'); ?></a></li>
    <li class="navigation-menu-item-right"><a href="<?php echo site_url('admin/main'); ?>" class="navigation-item"><?php echo $this->lang->line('menu.admin'); ?></a></li>
