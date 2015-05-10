07794e217f30c3376b45abdd74b360a7ea2174cf
nvcleemp <nico.vancleemput@gmail.com>
2015-05-10 16:22:11 +0200

    Added localised names for the membership types

---
+++ site/application/language/dutch/membershiptypes_lang.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('Directe toegang tot scripts is niet toegestaan');
+
+$lang['membershiptypes.default_membership'] = 'Standaard lidmaatschap';
+$lang['membershiptypes.trial_membership'] = 'Proeflidmaatschap';
+$lang['membershiptypes.trial_lesson'] = 'Proefles';
---
+++ site/application/language/english/membershiptypes_lang.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['membershiptypes.default_membership'] = 'Standard membership';
+$lang['membershiptypes.trial_membership'] = 'Trial membership';
+$lang['membershiptypes.trial_lesson'] = 'Trial lesson';
