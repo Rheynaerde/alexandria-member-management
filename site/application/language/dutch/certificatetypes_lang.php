2e4d4e3ad0c0d9b3641b1523ef4593e52d460e65
nvcleemp <nico.vancleemput@gmail.com>
2015-05-12 15:43:42 +0200

    Added localised names for the certificate types

---
+++ site/application/language/dutch/certificatetypes_lang.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('Directe toegang tot scripts is niet toegestaan');
+
+$lang['certificatetypes.medical'] = 'Medisch attest';
+$lang['certificatetypes.non_active_administrative'] = 'Verklaring van niet-actief sporter: administratief lid';
+$lang['certificatetypes.non_active_referee'] = 'Verklaring van niet-actief sporter: scheidsrechter';
+$lang['certificatetypes.non_active_trainer'] = 'Verklaring van niet-actief sporter: lesgever';
---
+++ site/application/language/english/certificatetypes_lang.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['certificatetypes.medical'] = 'Medical certificate';
+$lang['certificatetypes.non_active_administrative'] = 'Declaration of non-active athlete: administrative member';
+$lang['certificatetypes.non_active_referee'] = 'Declaration of non-active athlete: referee';
+$lang['certificatetypes.non_active_trainer'] = 'Declaration of non-active athlete: trainer';
