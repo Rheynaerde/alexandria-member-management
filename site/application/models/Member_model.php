6f2f409a99263d4ca573c8ba091d384851793e72
nvcleemp <nico.vancleemput@gmail.com>
2015-05-14 21:54:48 +0200

    Added method to get active members

--- site/application/models/Member_model.php
+++ site/application/models/Member_model.php
@@ -3,6 +3,21 @@
 
 class Member_model extends CI_Model {
 
+    function get_active_members() {
+        $this->db->from('members');
+        $this->db->select('members.id, persons.firstName, persons.lastName, persons.birthdate, members.federation_id');
+        $this->db->select('gender.name as gender');
+        $this->db->select('hand.name as hand');
+        $this->db->join('persons', 'persons.id=members.person_id', 'left');
+        $this->db->join('gender', 'gender.id=persons.gender_id', 'left');
+        $this->db->join('hand', 'hand.id=members.hand_id', 'left');
+        $this->db->where('is_active', 1);
+        $this->db->order_by('persons.lastName', 'ASC');
+        $result = $this->db->get();
+
+        return $result;
+    }
+
     function all_members($only_names=true) {
         $this->db->from('members');
         if($only_names){
