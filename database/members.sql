e9bcd59a52f9444db956cdcc55e4f8a50f1963a1
nvcleemp <nico.vancleemput@gmail.com>
2015-05-14 21:24:46 +0200

    Added field to track whether member is still active.

--- database/members.sql
+++ database/members.sql
@@ -8,7 +8,8 @@ CREATE TABLE IF NOT EXISTS `members` (
   `id` int(11) NOT NULL,
   `person_id` int(10) unsigned NOT NULL,
   `federation_id` varchar(10) DEFAULT NULL,
-  `hand_id` int(11) NOT NULL
+  `hand_id` int(11) NOT NULL,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Set the indices for this table
@@ -16,7 +17,8 @@ ALTER TABLE `members`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `federationId` (`federationId`),
   ADD UNIQUE KEY `person_id` (`person_id`),
-  ADD KEY `hand` (`hand_id`);
+  ADD KEY `hand` (`hand_id`),
+  ADD KEY `is_active` (`is_active`);
 
 -- id should auto-increment
 ALTER TABLE `members`
