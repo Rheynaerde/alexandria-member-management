460cf0e76115751b2f1ebdbe0f9a2b0e4ff73146
nvcleemp <nico.vancleemput@gmail.com>
2015-05-24 11:59:08 +0200

    Added descriptions for the remaining membership types

--- site/application/language/dutch/membershiptypes_lang.php
+++ site/application/language/dutch/membershiptypes_lang.php
@@ -4,3 +4,7 @@ defined('BASEPATH') OR exit('Directe toegang tot scripts is niet toegestaan');
 $lang['membershiptypes.default_membership'] = 'Standaard lidmaatschap';
 $lang['membershiptypes.trial_membership'] = 'Proeflidmaatschap';
 $lang['membershiptypes.trial_lesson'] = 'Proefles';
+$lang['membershiptypes.second_club'] = 'Tweede club';
+$lang['membershiptypes.trainer'] = 'Trainer';
+$lang['membershiptypes.referee'] = 'Scheidsrechter';
+$lang['membershiptypes.administrative'] = 'Administratief lidmaatschap';
--- site/application/language/english/membershiptypes_lang.php
+++ site/application/language/english/membershiptypes_lang.php
@@ -4,3 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['membershiptypes.default_membership'] = 'Standard membership';
 $lang['membershiptypes.trial_membership'] = 'Trial membership';
 $lang['membershiptypes.trial_lesson'] = 'Trial lesson';
+$lang['membershiptypes.second_club'] = 'Second club';
+$lang['membershiptypes.trainer'] = 'Trainer';
+$lang['membershiptypes.referee'] = 'Referee';
+$lang['membershiptypes.administrative'] = 'Administrative membership';
