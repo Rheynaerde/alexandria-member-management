8c3eb9a56ff841a7100e144e405e836bc3912023
nvcleemp <nico.vancleemput@gmail.com>
2015-05-25 19:18:56 +0200

    Method to get all payments

--- site/application/models/Payment_model.php
+++ site/application/models/Payment_model.php
@@ -7,6 +7,19 @@ class Payment_model extends CI_Model {
                 $this->session->userdata('has_member_management_rights');
     }
     
+    function get_memos($paid=false, $cancelled=false) {
+        $this->db->from('payment_memos as pm');
+        $this->db->select('pm.id, date, due_date, is_paid, pm.is_cancelled, name, pm.description, giro_description');
+        $this->db->select('SUM(pi.amount) as amount', false);
+        $this->db->join('payment_items as pi', 'pm.id=memo_id', 'left');
+        $this->db->where('pi.is_cancelled', '0');
+        $this->db->where('pm.is_cancelled', $cancelled ? '1' : '0');
+        $this->db->where('pm.is_paid', $paid ? '1' : '0');
+        $this->db->group_by('pm.id');
+        $this->db->order_by('date', 'ASC');
+        return $this->db->get()->result();
+    }
+    
     function get_memo($memo_id) {
         $this->db->from('payment_memos as pm');
         $this->db->select('date, due_date, is_paid, pm.is_cancelled, name, pm.description, giro_description');
